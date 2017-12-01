48144ea3d4751612065ec7b1623def9edd8a9bd1
h3ys1n <33125562+h3ys1n@users.noreply.github.com>
2017-12-01 23:47:49 +0300

    add anim search

--- WS/src/scripts/adaptive_menu.js
+++ WS/src/scripts/adaptive_menu.js
@@ -3,8 +3,23 @@ $(function(){
         $(".menu").slideToggle(500)
     });
 
-    $('#js-search-trigger').on('click', function(){
-        $("#js-search").animate({width: 'toggle'});
+    $('#js-search-trigger--on').on('click', function(){
+        $(this).attr("id", "js-search-trigger--off");             
+        $('.nav').animate({marginTop: '30px'}, function(){
+            $("#js-search").animate({width: 'toggle'}, function() {
+                $('#js-search__button').animate({width: 'toggle'})
+            });
+        });      
     });
    
+    //reverse animation
+
+    $('#js-search-trigger--off').on('click', function() {
+        $(this).attr("id", "js-search-trigger--on");             
+        $('#js-search__button').animate({width: 'toggle'}, function() {
+            $("#js-search").animate({width: 'toggle'}, function() {
+                $('.nav').animate({marginTop: '0px'})
+            })
+        })
+    })
 })
