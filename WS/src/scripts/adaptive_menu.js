674fac95e04ec4f7c0c5dc457eead2e8209ee8d7
h3ys1n <33125562+h3ys1n@users.noreply.github.com>
2017-12-02 02:40:35 +0300

    add search reverse anim

--- WS/src/scripts/adaptive_menu.js
+++ WS/src/scripts/adaptive_menu.js
@@ -1,25 +1,27 @@
-$(function(){
+$(document).ready(function(){
+    
     $('#js-menu-trigger').on('click', function(){
         $(".menu").slideToggle(500)
     });
 
-    $('#js-search-trigger--on').on('click', function(){
-        $(this).attr("id", "js-search-trigger--off");             
-        $('.nav').animate({marginTop: '30px'}, function(){
-            $("#js-search").animate({width: 'toggle'}, function() {
-                $('#js-search__button').animate({width: 'toggle'})
-            });
-        });      
-    });
-   
-    //reverse animation
-
-    $('#js-search-trigger--off').on('click', function() {
-        $(this).attr("id", "js-search-trigger--on");             
-        $('#js-search__button').animate({width: 'toggle'}, function() {
-            $("#js-search").animate({width: 'toggle'}, function() {
-                $('.nav').animate({marginTop: '0px'})
+        
+    $('#js-search-trigger').on('click', function(){
+        if($(this).is('.js-search-trigger--on')) {
+            $('#js-search__button').animate({width: 'toggle'}, function() {
+                $("#js-search").animate({width: 'toggle'}, function() {
+                    $('.nav').animate({marginTop: '0'})
+                })
             })
-        })
-    })
+            $(this).removeClass('js-search-trigger--on');                       
+        }
+        else {
+            $('.nav').animate({marginTop: '30px'}, function(){
+                $("#js-search").animate({width: 'toggle'}, function() {
+                    $('#js-search__button').animate({ width: 'toggle'})
+                })
+            })  
+            $(this).addClass('js-search-trigger--on');   
+        }
+    });
+
 })
