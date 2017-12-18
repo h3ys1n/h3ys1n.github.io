0f3ad60817ba5a132dc9a137eec013dad35666f4
h3ys1n <33125562+h3ys1n@users.noreply.github.com>
2017-12-18 11:53:01 +0300

    js css minify

--- MLP/build/scripts/anchorMenu.js
+++ MLP/build/scripts/anchorMenu.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('a[href*=#]').on("click", function(e){
+    $('a[href*="#"]').on("click", function(e){
     var anchor = $(this);
     $('html, body').stop().animate({
-    scrollTop: $(anchor.attr('href')).offset().top-78
+    scrollTop: $(anchor.attr('href')).offset().top
     }, 800);
     e.preventDefault();
     });
